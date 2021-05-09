13426600f0a21048cf0d1f41eb09b1bed98d14b0
iamShekharGH <iamShekharGH@users.noreply.github.com>
2021-05-09 23:16:51 +0530

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,8 @@
 
 
-$(document).ready(function(){
-	$('.main-page-logo').hide().delay(300).show(300);
-	$('.container').hide().show(300);
-	$('.sociallinks').animate({left:"400em"});
-	//$('.wrapper').hide().delay(650).slideDown(300);
-});
+// $(document).ready(function(){
+// 	$('.main-page-logo').hide().delay(300).show(300);
+// 	$('.container').hide().show(300);
+// 	$('.sociallinks').animate({left:"400em"});
+// 	//$('.wrapper').hide().delay(650).slideDown(300);
+// });
